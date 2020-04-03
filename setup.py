8ad19ffbd18adcfb2df37e5b51fa24f58ff6f974
Peter Holtermann <peter.holtermann@io-warnemuende.de>
2020-04-03 11:33:38 +0200

    pip

--- setup.py
+++ setup.py
@@ -5,9 +5,15 @@ ROOT_DIR='pylatlon'
 with open(os.path.join(ROOT_DIR, 'VERSION')) as version_file:
     version = version_file.read().strip()
 
+# read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='pylatlon',
       version=version,
       description='Tools to handle geographic positions (parsing, gui input)',
+      long_description_content_type='text/x-rst',
       url='https://github.com/MarineDataTools/pylatlon',
       author='Peter Holtermann',
       author_email='peter.holtermann@io-warnemuende.de',
@@ -15,8 +21,8 @@ setup(name='pylatlon',
       packages=['pylatlon'],
       scripts = [],
       entry_points={},
+      install_requires=[ 'pyproj'],
       package_data = {'':['VERSION']},
       zip_safe=False)
 
 
-# TODO Depends on pyproj
